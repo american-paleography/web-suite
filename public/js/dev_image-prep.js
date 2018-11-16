9c9739035b60a17a63e7c25a41d409882628a98c
Ethan Kaminski (from dev server) <ethan.kaminski@gmail.com>
2018-11-16 13:00:19 +0000

    Work on matching images to text

--- public/js/dev_image-prep.js
+++ public/js/dev_image-prep.js
@@ -3,7 +3,7 @@ $(function() {
 	$.get('/dev/images/get-data', function(data) {
 		holder = $('#output');
 		data.forEach(line => {
-			payload = $('<p>');
+			var payload = $('<p>');
 
 			for (var prop of ['text']) {
 				var obj = $('<span>');
@@ -19,7 +19,37 @@ $(function() {
 
 			loadLineImage(path, line, canvas, function() {
 				setTimeout(function() {
-					render_debug_info_for_line(canvas[0]);
+					var chunks = render_debug_info_for_line(canvas[0]);
+					var words = line.text.split(/\s+/);
+
+					// VERY naive attempt at matching images to words
+					if (chunks.length > words.length) {
+						// naive approach: discard the narrowest images, since they're probably a mistaken crop
+						var widths = chunks.map(c => c.canvas.width).sort((a,b) => a-b);
+						var excess = chunks.length - words.length;
+						var trimThreshold = widths[excess];
+						chunks = chunks.filter(c => c.canvas.width >= trimThreshold);
+					}
+					
+					var guessHolder = $('<ul>');
+					for (var i = 0; i < words.length; ++i) {
+						var guess = $('<li>');
+						guess.text(words[i]);
+						if (chunks[i]) {
+							var newImg = $('<img>');
+							var oldCanvas = chunks[i].canvas;
+							guess.text(words[i] + `(${chunks[i].canvas.width})`);
+							newImg.attr('src', oldCanvas.toDataURL());
+							guess.append(newImg);
+						}
+						guessHolder.append(guess);
+					}
+					payload.append(guessHolder);
+					if (trimThreshold) {
+						var tmp = $('<span>');
+						tmp.text([widths[0], trimThreshold, widths[widths.length-1]].join(', '));
+						payload.append(tmp);
+					}
 				}, 200);
 			});
 
