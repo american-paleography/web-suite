7c3cf6a8e58ede0819de57ddd4c9998b1eda353b
Ethan Kaminski (from dev server) <ethan.kaminski@gmail.com>
2019-02-05 17:19:02 +0000

    Remove some old (orphaned) code

--- frontend-src/exhibit-concordance.js
+++ frontend-src/exhibit-concordance.js
@@ -2,52 +2,6 @@
 
 const e = React.createElement;
 
-function cutPolygon(points, source, buffer, output) {
-	var rect = getBoundingBox(points);
-
-	if (rect[2] == 0 || rect[3] == 0) {
-		return false;
-	}
-
-	buffer.canvas.width = source.canvas.width;
-	buffer.canvas.height = source.canvas.height;
-
-	buffer.ctx = buffer.canvas.getContext('2d');
-	buffer.ctx.globalCompositeOperation = 'source-over';
-
-	buffer.ctx.putImageData(source.ctx.getImageData(...rect), rect[0], rect[1]);
-
-	buffer.ctx.globalCompositeOperation = 'destination-in';
-	buffer.ctx.beginPath();
-	buffer.ctx.moveTo(...points[points.length-1]);
-	points.forEach(point => {
-		buffer.ctx.lineTo(...point);
-	})
-
-	buffer.ctx.fill();
-
-	var clipped = buffer.ctx.getImageData(...rect);
-
-	output.canvas.width = clipped.width;
-	output.canvas.height = clipped.height;
-
-	output.ctx.putImageData(clipped, 0, 0);
-
-	return true;
-}
-
-function getBoundingBox(points) {
-	var x = points.map(a => a[0]).sort((a,b) => a-b); // um, why is the default sort string-y?
-	var y = points.map(a => a[1]).sort((a,b) => a-b);
-	console.log(points, x, y);
-	var left = x[0];
-	var top = y[0];
-	
-	var width = x[x.length-1] - left;
-	var height = y[y.length-1] - top;
-
-	return [left, top, width, height];
-}
 
 $(function() {
 	window.onerror = alert;
@@ -286,53 +240,6 @@ class AnnotatedWordImage extends React.Component {
 					{poly.location}
 				</td>
 			</tr>
-		)
-	}
-}
-
-class WordImage extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			
-		}
-	}
-
-	componentDidMount() {
-		const {canvas, img} = this.refs;
-		const points = this.props.points;
-
-		const ctx = canvas.getContext('2d');
-
-		img.onload = function() {
-			var source = { canvas: $('<canvas>')[0] };
-			source.ctx = source.canvas.getContext('2d');
-
-			source.canvas.width = img.width;
-			source.canvas.height = img.height;
-			source.ctx.drawImage(img, 0, 0);
-
-			var buffer = { canvas: $('<canvas>')[0] };
-			buffer.ctx = buffer.canvas.getContext('2d');
-
-			var output = {
-				canvas: canvas,
-				ctx: ctx,
-			}
-
-			cutPolygon(points, source, buffer, output);
-		}
-	}
-
-	render() {
-		var path = this.props.path;
-		var src = 'http://image-store.tpen-demo.americanpaleography.org/' + path;
-		return (
-			<div>
-				<canvas ref="canvas" />
-				<img ref="img" src={src} crossOrigin="Anonymous" class="hidden" />
-			</div>
 		)
 	}
 }
