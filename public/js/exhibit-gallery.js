7aa2bf16e774c7f249653320f16032f0466abd53
Ethan Kaminski (from dev server) <ethan.kaminski@gmail.com>
2019-02-06 11:53:43 +0000

    Add some CSS classes to word cutouts gallery HTML

--- public/js/exhibit-gallery.js
+++ public/js/exhibit-gallery.js
@@ -1,10 +1,14 @@
 $(function() {
 	const ajax_path = $('[name=ajax-path]').val();
 	$('#letters > .navbubble').on('click', function() {
+		$('.navbubble.active').removeClass('active');
+		$(this).addClass('active');
 		var out = $('#gallery-output');
 		out.empty();
 		$.get('/winterthur/ajax/polygon-list/' + ajax_path + '?begins_with=' + $(this).data('letter'), function(data) {
 			out[0].innerHTML = makeGallery({polygons: data.polygons, base_path: "http://webtools.americanpaleography.org/poly-images/"});
 		})
 	})
+
+	$('.navbubble[data-letter=a]').click();
 })
