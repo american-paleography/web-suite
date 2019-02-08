55da08f23b83bbc07ea3a6255ac26d21348b49cc
Ethan Kaminski (from dev server) <ethan.kaminski@gmail.com>
2019-02-08 11:55:08 +0000

    Load images in a specific order

--- public/js/exhibit-gallery.js
+++ public/js/exhibit-gallery.js
@@ -1,9 +1,31 @@
 $(function() {
 	const ajax_path = $('[name=ajax-path]').val();
+	const out = $('#gallery-output');
+
+	$(document).on('load', 'img', function() {
+		$(this).removeClass('loading');
+		$(this).addClass('loaded');
+		var count = $('.loading').length
+		var add = 10 - count;
+
+		alert(add);
+
+		if (add > 0) {
+			$('.waiting').each((i, el) => {
+				if (i >= add) {
+					return;
+				}
+
+				$(el).removeClass('waiting');
+				$(el).addClass('loading');
+				el.src = $(el).data('path');
+			})
+		}
+	})
+
 	$('#letters > .navbubble').on('click', function() {
 		$('.navbubble.active').removeClass('active');
 		$(this).addClass('active');
-		var out = $('#gallery-output');
 		out.empty();
 		$.get('/winterthur/ajax/polygon-list/' + ajax_path + '?begins_with=' + $(this).data('letter'), function(data) {
 			out[0].innerHTML = makeGallery({polygons: data.polygons, base_path: "http://webtools.americanpaleography.org/poly-images/"});
@@ -11,4 +33,29 @@ $(function() {
 	})
 
 	$('.navbubble[data-letter=a]').click();
+
+	alert('test');
 })
+
+function lazyLoad(el) {
+	const out = $('#gallery-output');
+	$(el).removeClass('loading');
+	$(el).addClass('loaded');
+	var count = $('.loading').length
+	var add = 10 - count;
+
+	var onload = $(el).attr('onload');
+
+	if (add > 0) {
+		$('.waiting').each((i, el) => {
+			$(el).attr('onload', onload)
+			if (i >= add) {
+				return;
+			}
+
+			$(el).removeClass('waiting');
+			$(el).addClass('loading');
+			el.src = $(el).data('path');
+		})
+	}
+}
